0fe66cdd8d2953f2c1b4e92936b42cb7f2bf6382
imnddat <nddat2002@gmail.com>
2023-07-30 17:29:45 +0700

    Lan dau lam chuyen ay-Lan dau dua code len Server

---
+++ src/com/fu/mathutil/MathUtil.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.fu.mathutil;
+
+/**
+ *
+ * @author NguyenDucDat
+ */
+public class MathUtil {
+
+    public static long getFactorial(int n) {
+        if (n < 0 || n > 20) {
+            throw new IllegalArgumentException("Invalid argument. n must be between 0 and 20.");
+        }
+
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        long product = 1;
+        for (int i = 2; i <= n; i++) {
+            product = product * i;
+        }
+        return product;   
+    }
+}
---
+++ src/com/fu/mathutil/MathUtilLive.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.fu.mathutil;
+
+/**
+ *
+ * @author NguyenDucDat
+ */
+public class MathUtilLive {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       long result = MathUtil.getFactorial(5);
+        System.out.println("5! = 120; actual: " + result);
+        System.out.println("6! = 720; actual: " + MathUtil.getFactorial(6));
+        System.out.println("0! = 1; actual: " + MathUtil.getFactorial(1));
+    }
+    
+}
