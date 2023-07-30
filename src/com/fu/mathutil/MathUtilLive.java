4715a54b9af7bf07529b2ef8a2378c4134d22e0c
imnddat <nddat2002@gmail.com>
2023-07-30 22:08:43 +0700

    Lan thu 2 day code len Server

--- src/com/fu/mathutil/MathUtilLive.java
+++ src/com/fu/mathutil/MathUtilLive.java
@@ -17,7 +17,9 @@ public class MathUtilLive {
        long result = MathUtil.getFactorial(5);
         System.out.println("5! = 120; actual: " + result);
         System.out.println("6! = 720; actual: " + MathUtil.getFactorial(6));
-        System.out.println("0! = 1; actual: " + MathUtil.getFactorial(1));
+        System.out.println("0! = 1; actual: " + MathUtil.getFactorial(0));
+        System.out.println("1! = 1; actual: " + MathUtil.getFactorial(1));
+        System.out.println("3! = 6; actual: " + MathUtil.getFactorial(3));
     }
     
 }
